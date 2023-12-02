9fdc46070db23f08933322e043005c4240d8bc68
0SNP0 <36269903+0SNP0@users.noreply.github.com>
2023-12-02 21:24:19 +0300

    solution

--- ITMO_DZ_4/ThreadSafeArray.swift
+++ ITMO_DZ_4/ThreadSafeArray.swift
@@ -2,20 +2,32 @@ import Foundation
 
 class ThreadSafeArray<T>  {
     private var array: [T] = []
+    private let lock = RWLock()
+    
+    required init(_ elements: T...) {
+        array = Array(elements)
+    }
 }
 
 extension ThreadSafeArray: RandomAccessCollection {
     typealias Index = Int
     typealias Element = T
 
-    var startIndex: Index { return array.startIndex }
-    var endIndex: Index { return array.endIndex }
+    var startIndex: Index { lock.readAction { array.startIndex } }
+    var endIndex: Index { lock.readAction { array.endIndex } }
 
     subscript(index: Index) -> Element {
-        get { return array[index] }
+        get { lock.readAction { array[index] } }
+        set { lock.writeAction { array[index] = newValue } }
     }
 
     func index(after i: Index) -> Index {
-        return array.index(after: i)
+        return lock.readAction { array.index(after: i) }
+    }
+}
+
+extension ThreadSafeArray {
+    func append(_ newElement: Element) {
+        lock.writeAction { array.append(newElement) }
     }
 }
--- ITMO_DZ_4/main.swift
+++ ITMO_DZ_4/main.swift
@@ -1,9 +1,13 @@
 import Foundation
 
-let ff = ThreadSafeArray<Int>()
-var dd = [1]
-dd.append(1)
-ff.dropFirst()
+let queue = DispatchQueue(label: "writeArray", attributes: .concurrent)
 
+let ff = ThreadSafeArray<Int>(1, 2, 3)
 let sd = ff[0]
+print(ff.count)
 
+for i in 0..<100 {
+    queue.async {
+        ff.append(i)
+    }
+}
